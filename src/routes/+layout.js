bc73393a9d65a5fd3df4f31a4983dd5c72cdf269
derharry <derharry@users.noreply.github.com>
2025-02-28 00:27:24 +0100

    use .env.build<any> with bun run build --mode build<any>

---
+++ src/routes/+layout.js
@@ -0,0 +1,5 @@
+//src/routes/+layout.js -->
+export const prerender = true;
+export const ssr = false;
+export const csr = true;
+export const trailingSlash = 'always';
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+
+export default defineConfig(({ mode }) => {
+	console.log('Current mode:', mode); // Logs "build.zp" when --mode build.zp is used
+	process.env = loadEnv(mode, process.cwd(),'');
+	return {
+		plugins: [sveltekit()]
+	};
 });
