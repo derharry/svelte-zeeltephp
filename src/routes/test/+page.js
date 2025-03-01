f40df1925a3f20f93bce6cf366a327a6d14c5f9c
derharry <derharry@users.noreply.github.com>
2025-03-01 15:27:30 +0100

    text correction

--- src/routes/test/+page.js
+++ src/routes/test/+page.js
@@ -5,7 +5,7 @@ export async function load({ params, fetch, url }) {
       console.log('load test/+page.js', url)
       const res_php = await fetch_api(fetch, url);
       return {
-            res_page: 'hello from +page.js',
+            res_page: 'hello from /test/+page.js',
             res_php2:  res_php.data.res_php2
       }
 }
