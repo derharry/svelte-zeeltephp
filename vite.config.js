16b716b76f3769a6879fd5d4b8d8782327b2ae35
Harry <10669513+derharry@users.noreply.github.com>
2025-04-29 21:59:10 +0200

    init version 0.0.9

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig, loadEnv } from 'vite';
-import {zeeltephp} from '$lib/vite-plugin/zeeltephp-vite-plugin.js';
+import { zeeltephp } from '$lib/vite-plugin/zeeltephp-vite-plugin.js';
 
 export default defineConfig(({ mode }) => {
       console.log('Current mode:', mode); // Logs "build.zp" when --mode build.zp is used
