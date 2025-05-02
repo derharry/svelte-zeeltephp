5065a5b2745f1b961df0d1f468d5da9bd600c413
Harry <10669513+derharry@users.noreply.github.com>
2025-05-02 19:50:13 +0200

    exposed VarDump.svelte Component to use from ZeeltePHP

--- dist/index.js
+++ dist/index.js
@@ -2,3 +2,4 @@
 export { ZP_EventDetails, get_event_action_details } from "./zeeltephp/class.zp.eventdetails.js";
 export { ZP_ApiRouter } from "./zeeltephp/class.zp.apirouter.js";
 export { zp_fetch_api } from "./zeeltephp/zp.fetch.api.js";
+export { VarDump } from "./zeeltephp/VarDump.svelte";
