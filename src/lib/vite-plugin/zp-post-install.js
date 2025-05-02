3a7193189b8530a0a70a9a7efa596cee33a8ef1a
Harry <10669513+derharry@users.noreply.github.com>
2025-05-02 09:02:07 +0200

    rc1

---
+++ src/lib/vite-plugin/zp-post-install.js
@@ -0,0 +1,19 @@
+// create-static-api.js
+import { mkdir } from 'node:fs/promises'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+async function createStaticApiFolder() {
+  const folderPath = `${__dirname}/static/api`
+  
+  try {
+    await mkdir(folderPath, { recursive: true })
+    console.log('Created static/api directory')
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err
+  }
+}
+
+createStaticApiFolder()
