907ee6b20b69308bf55e9c12d65f7418eda4d7b2
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-05-19 16:44:19 +0200

    documentation, include-path now points to init.php.

--- src/lib/templates/static/api/index.php
+++ src/lib/templates/static/api/index.php
@@ -1,13 +1,20 @@
 <?php
 #
-# index
+#  ZeeltePHP – /api Working Directory
 #
-#     as init loader for static-url/api/
-#     api can be any naming
+#  This path is reserved for ZeeltePHP.
 #
-
-## path to include
-include('../../node_modules/zeeltephp/dist/index.php');
-
-
+#  Usage:
+#    - npm run dev   : ZeeltePHP is loaded from its library package (node_modules/zeeltephp/dist/api)
+#    - npm run build : ZeeltePHP post-build finalizes the /build/api directory, including your PHP files.
+#
+#  Customizing api/index.php:
+#    - ZeeltePHP relies on this specific path structure.
+#    - If you need to customize this file, ensure ZeeltePHP can co-exist with your custom API logic.
+#    - To integrate ZeeltePHP, simply include init.php as shown below. 
+#    - If you add logic after including init.php, use `chdir(__DIR__);` to restore the current working directory.
+#
+#  Start ZeeltePHP:
+include('../../node_modules/zeeltephp/dist/api/init.php');
+# chdir(__DIR__); // Uncomment if you implement logic after init.php.
 ?>
