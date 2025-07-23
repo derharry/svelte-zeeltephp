2602bff92dd720620eac16f5a2df6ee54c524891
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-07-23 02:28:20 +0200

    added preflight for X-ZPC-PAGE and X-ZPC-API

--- static/api/zeeltephp/init.php
+++ static/api/zeeltephp/init.php
@@ -13,7 +13,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS');
-    header('Access-Control-Allow-Headers: X-ZPC-Api, Content-Type');
+    header('Access-Control-Allow-Headers: X-ZPC-PAGE, X-ZPC-API, Content-Type');
     exit(0);
 }
 
@@ -52,7 +52,7 @@ register_shutdown_function(function() {
 });
 
 // Enable ZeeltePHP debugging (set to false in production)
-define('ZP_DEBUG', true);
+define('ZP_DEBUG', false);
 
 // #####################################
 // ## main()
