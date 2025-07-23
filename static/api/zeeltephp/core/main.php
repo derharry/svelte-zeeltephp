6e80828acd90c4cc4ac608cae63291981e5d6f1b
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-07-23 02:34:55 +0200

    changed logic to +.*.server.php files

--- static/api/zeeltephp/core/main.php
+++ static/api/zeeltephp/core/main.php
@@ -5,12 +5,10 @@
  * Start main() after zeeltephp_loadRunEnvironment() has set the environment paths (consts).
  */
 function zeeltephp_main() {
-     global $debug, $zpAR, $env, $db, $data;
+     global $debug, $zpAR, $env, $db, $data, $zpTime;
      try {
+          $zpTime->start('main()');
           zp_log_debug('zeeltephp_main()');
-
-          // hello at ZeeltePHP
-          // I should be ready-to-run in my environment now
 
           // load the .env-file or auto generate missing variables 
           //   equivalent to vite-plugin/zeeltephp_loadEnv
@@ -19,8 +17,12 @@ function zeeltephp_main() {
           // load the ApiRouter deparse @zeeltephp:api-request
           $zpAR = new ZP_ApiRouter($env);
 
-          if ($zpAR->route) {
-               // yuhu - all is set to exec +.php files
+          // 2025-07-17 1.0.4 support for context page/api
+          // changing to routeFiles[] instead routeFileExist + routeFile
+          if (sizeof($zpAR->routeFiles) > 0) {
+
+               //zp_log_debug("  route files:");
+               //zp_log_debug($zpAR->routeFiles);
 
                // # load consumer lib files
                zp_load_lib_files(PATH_ZPLIB);
@@ -31,19 +33,32 @@ function zeeltephp_main() {
                     $db = new ZP_DB($env['ZEELTEPHP_DATABASE_URL']);
                }
 
+               // 2025-07-16 1.0.4 support for +hooks.server.php
+               // maybe - hooks/handle also needs to be in a UniqFile-NameSpace
+               $hooksData;
+               if (is_file(PATH_ZPROUTES."+hooks.server.php")) {
+                    $zpTime->start('+hooks.server.php');
+                    include(PATH_ZPROUTES."+hooks.server.php");
+                    zp_log_debug('+hooks.server.php found.');
+                    if (function_exists('handle')) {
+                         zp_log_debug('executing handle()');
+                         $hooksData = handle();
+                    } else zp_log_debug('No METHOD handle() found.');
+                    zp_log_debug($zpTime->endN('+hooks.server.php'));
+               }
+               
                // init response of +.php files
-               $data = null;
-
-               // # run +page.server.php
-               //   zpAr now collects all +.php files but for now we still support just page.server.php
-               if ($zpAR->routeFileExist) {
-
-                    include('core/exec.page.server.php');
-                    $data = zp_exec_pageServerPHP();
+               $data = 'x';
+               
+               // parse the route
+               include_once('core/zp.executor.php');
+               foreach ($zpAR->routeFiles as $plusPhpFile) {
+                    $zpTime->start($plusPhpFile);
+                    $data = zp_executor($zpAR->routeBase, $plusPhpFile, $data);
+                    zp_log_debug($zpTime->endN($plusPhpFile));
                }
 
-               // # yuhu - all is good :-)
-               //   return data as JSON response, and let zp_fetch_api() do the rest :-)
+               // return data as JSON response, and let zp_fetch_api() do the rest :-)
                echo json_encode([
                     'ok'   => true,
                     'code' => 200,
@@ -51,8 +66,9 @@ function zeeltephp_main() {
                     //'zpDB' => $db
                     // -- ...$data // pitfall - do not because of send-data-JSON of any data  
                ]);
-          }
-          else {
+
+          } else {
+               // error handling .. no +php-files to exec found
                // wel - lets notifiy what is missing
                if (!$zpAR->route) {
                     // nothing to do - just no route is given
@@ -69,8 +85,8 @@ function zeeltephp_main() {
                     throw new Error(501);
                }
           }
-
           zp_log_debug('//zeeltephp_main()');
+          zp_log_debug($zpTime->endN('main()'));
 
      } catch (Error $exp) {  
           zp_handle_error($exp); //, "Error");
