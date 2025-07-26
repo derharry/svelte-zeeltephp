b3837ecfc30cdfae8fa67df4624cc254a5fd0451
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-07-27 00:31:46 +0200

    in development

--- static/api/zeeltephp/core/exec.hooks.server.php
+++ static/api/zeeltephp/core/exec.hooks.server.php
@@ -1,8 +1,15 @@
 <?php
 
      function zp_exec_hooksServerPHP() {
-      
-         
+          global $zpAR, $data;
+          zp_log_debug('zp_exec_ServerPHP()');
+
+          if (function_exists('handle')) {
+               zp_log_debug("execute handle()");
+               return $callbackFunction();
+          }
+
+          zp_log_debug("No handle found.");
 
      }
 
