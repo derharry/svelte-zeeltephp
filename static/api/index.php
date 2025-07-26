13186bf11f6e9b1b6cc793ace741f06d7592b0f6
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-07-27 00:25:57 +0200

    respond to browser preflight request

--- static/api/index.php
+++ static/api/index.php
@@ -1,4 +1,12 @@
 <?php
 # ZeeltePHP - main API entry point 
+// respond to browser preflight request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+     header('Access-Control-Allow-Origin: * ');
+     header('Access-Control-Allow-Methods: *');
+     header('Access-Control-Allow-Headers: X-ZPC-PAGE, X-ZPC-API, CONTENT-TYPE');
+     exit(0);
+}
+// initialize and load ZeeltePHP
 include('zeeltephp/init.php');
 ?>
