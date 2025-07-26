a541b26f56e6dac6c45b755f2b1f3be07dd47ce7
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-07-27 01:02:49 +0200

    added support for CLI/php.exe. json_response is now returned as is

--- src/lib/zeelte/zp.fetch.api.js
+++ src/lib/zeelte/zp.fetch.api.js
@@ -15,13 +15,10 @@ export function zp_get_eventDetails(event) {
 }
 
 /**
- * Placeholder for future SvelteKit-native fetch integration.
- * Intended to provide a drop-in replacement for zp_fetch_api() using SvelteKit's conventions.
- * @param {Function} fetch - SvelteKit's fetch function
- * @param {*} dataOrEvent  - Data or event to send
+ * 1.0.4 - Placeholder for future SvelteKit-native fetch integration.(instead of zp_fetch_api)
  */
-export function fetch_api(fetch, dataOrEvent) {
-    // To be implemented for SvelteKit-native backend fetch
+export function zp_fetch(fetch, router, data = undefined, method = undefined, headers = undefined, debug = false) {
+    zp_fetch_api(fetch, router, data, method, headers, debug);
 }
 
 /**
@@ -47,7 +44,7 @@ export function fetch_api(fetch, dataOrEvent) {
  *   *not yet* zp_fetch_api(fetch, string URL [, Data]) If your route or GET string (in near future: prefix + to fetch from +api.php).
  *   ... more tbd
  * 
- * @param {Function} fetch - SvelteKit's fetch function; cannot be imported seperatly
+ * @param {Function} fetch - SvelteKit's fetch function (include from +page.js or +page.svelte) (connot be imported seperatly)
  * @param {ZP_ApiRouter|ZP_EventDetails|Event|URL|URLParams|string} router - Router, event, or URL describing the request
  * @param {*}      [data] - Optional, force data to send with the request
  * @param {string} [method] - Optional, force used HTTP method (GET, POST, etc.)
@@ -79,20 +76,25 @@ export function zp_fetch_api(fetch, router, data = undefined, method = undefined
             // Perform the fetch to ZeeltePHP backend
             fetch(zpar.fetch_url, zpar.fetch_options)
                 .then(response => response.json()) // Resolve the json-response
-                .then(rawData => {
-                    // If backend returned { ok: true, data: ... }, resolve with data
-                    if (rawData.ok && 'data' in rawData) {
-                        // Received data response from +.php
-                        resolve(rawData.data);
-                        // -- if (debug) console.log('ok', rawData.data);
-                        // -- idea: Push data to $page.data or $page.form or $page.error
-                    } 
-                    // Otherwise, resolve the raw response (may be an Error-Notification, ..)
-                    else {
-                        resolve(rawData);
-                        // -- if (debug) console.error(rawData);
-                        // -- idea: Push data to $page.data or $page.form or $page.error
-                    }
+                .then(json => {
+                    // -- idea: Push data to $page.data or $page.form or $page.error
+                    // 1.0.4 - just return the JSON response
+                    resolve(json);
+                    /*
+                        // previous <1.0.3
+                        // If backend returned { ok: true, data: ... }, resolve with data
+                        if (json !== null) {
+                            // Received data response from +.php
+                            // -- if (debug) console.log('ok', rawData.data);
+                            resolve(rawData.data);
+                        } 
+                        // Otherwise, resolve the raw response (may be an Error-Notification, ..)
+                        else {
+                            resolve(json);
+                            // -- if (debug) console.error(rawData);
+                            // -- idea: Push data to $page.data or $page.form or $page.error
+                        }
+                    */
                 })
                 .catch(error => {
                     console.error('zp_fetch_api/promise', { error });
