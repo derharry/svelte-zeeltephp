dcfe18b6f71602c77fc7a1a569bbfb5db0ccbc18
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-08-01 03:39:13 +0200

    added export zp_fetch, HTML_Marquee

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,10 @@
 // index.js
 // Reexport your entry components here
-export { zp_fetch_api } from './zeelte/zp.fetch.api';
+export { zp_fetch, zp_fetch_api } from './zeelte/zp.fetch.api';
 export { zp_page_route } from './zeeltephp/zp.tools.js';
 export { ZP_ApiRouter } from './zeelte/class.zp.apirouter.js';
 export { ZP_EventDetails } from './zeelte/class.zp.eventdetails.js';
+export { default as HTML_Marquee } from './zeelte/HTML_Marquee.svelte';
 export { default as Loader } from './zeelte/Loader.svelte';
 export { default as ZPDev } from './zeeltephp/ZPDev.svelte';
 export { default as VarDump } from './zeelte/VarDump.svelte';
