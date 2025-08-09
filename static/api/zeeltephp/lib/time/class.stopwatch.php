7858b6937a06bab9736bcbfcb122e5c96f29d4ca
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-08-10 00:52:34 +0200

    updated logic

--- static/api/zeeltephp/lib/time/class.stopwatch.php
+++ static/api/zeeltephp/lib/time/class.stopwatch.php
@@ -1,27 +1,62 @@
 <?php namespace ZeeltePHP\Lib\Time;
 
+class Watch {
+
+     public $name;
+     public $start;
+     public $stop;
+     public $labs = [];
+
+     /**
+      * 
+      */
+     function __construct($name = null, $start = false) {
+          $this->name = $name; # todo: ?? uuid_tiny(); # generate a name
+          return $start && $this->Start() ?? $start;
+     }
+
+     function Start() {
+          return $this->start = microtime(true);
+     }
+
+     function Stop() {
+          return $this->stop = microtime(true);
+     }
+     
+     function AddLab($name = null) {
+          $this->labs[] = new Lab($name);
+     }
+}
+
+class Lab {
+     public $name;
+     public $time;
+     function __construct($name = '') {
+          $this->name = $name;
+          $this->time = microtime(true);
+     }
+}
 
 class StopWatch {
 
-     public $timediff_register = [];
+     public $watches = [];
 
-
-     function start($title) {
-          $this->timediff_register[$title]['start'] = microtime(true);
+     function start($name) {
+          $this->watches[$name]['start'] = microtime(true);
      }
 
-     function end($title) {
-          $this->timediff_register[$title]['end'] = microtime(true);
-          return $this->getTimeDiff($title);
+     function end($name) {
+          $this->watches[$name]['end'] = microtime(true);
+          return $this->getTimeDiff($name);
      }
 
-     function endN($title) {
-          $this->timediff_register[$title]['end'] = microtime(true);
-          return "TM $title: ". $this->getTimeDiff($title);
+     function endN($name) {
+          $this->watches[$name]['end'] = microtime(true);
+          return "TM $name: ". $this->getTimeDiff($name);
      }
 
-     function getTimeDiff($title, $how = 'H:m:s') {
-          $diff = $this->timediff_register[$title]['end'] - $this->timediff_register[$title]['start'];
+     function getTimeDiff($name, $how = 'H:m:s') {
+          $diff = $this->watches[$name]['end'] - $this->watches[$name]['start'];
           return $diff;
           $seconds = (int)$diff;
           $micro = (int)(($diff - $seconds) * 1e6);
