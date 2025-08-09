ec87cf4fa03e16cd2bf95a16d563b9cf4cfae18c
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-08-10 00:41:14 +0200

    corrections

--- src/routes/(usage_examples)/zp_fetch_API/+page.svelte
+++ src/routes/(usage_examples)/zp_fetch_API/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 
      import { onMount } from "svelte";
-     import { zp_fetch, zp_fetch_api, HTML_Marquee } from "zeeltephp"
+     import { zp_fetch, HTML_Marquee } from "zeeltephp"
 
      export let data
 
@@ -14,65 +14,45 @@
 
 </script>
 
+<h1>zp_fetch context for API-requests</h1>
+<p>Here you find showcases for API requests like SvelteKit.</p>
+
 <div class="grid-container">
 
-     
-     <button 
-          on:click={() => {   
-               //data = zp_fetch(fetch, 'api/apidemo', 'GET', null, true)   
-               promise = zp_fetch('/zpdev', { method: 'GET' })   
-          }}>GET</button>
+     <button on:click={() => {   promise = zp_fetch('/zpdev', { method: 'GET' })   }}>GET</button>
      <span>
           Execute GET
      </span>
-
-
 
      <button on:click={() => {   promise = zp_fetch('/zpdev', { method: 'POST' })   }}>POST</button>
      <span>
           Execute POST
      </span>
 
-
-
      <button on:click={() => {   promise = zp_fetch('/zpdev', { method: 'PUT' })   }}>PUT</button>
      <span>
           Execute PUT
      </span>
-
-
 
      <button on:click={() => {   promise = zp_fetch('/zpdev', { method: 'PATCH' })   }}>PATCH</button>
      <span>
           Execute PATCH
      </span>
 
-
-
      <button on:click={() => {   promise = zp_fetch('/zpdev', { method: 'DELETE' })   }}>DELETE</button>
      <span>
           Execute DELETE
      </span>
 
-
-
-     <button 
-          on:click={() => {   
-               promise = zp_fetch('/zpdev', { method: 'HEAD', fetch: fetch })   
-          }}
-
-          >HEAD</button>
+     <button on:click={() => {   promise = zp_fetch('/zpdev', { method: 'HEAD', fetch: fetch }) }}>HEAD</button>
      <span>
           Execute HEAD
      </span>
-
-
 
      <button on:click={() => {   promise = zp_fetch('/zpdex', { method: 'POST' })   }}>none</button>
      <span>
           Execute a non existing route
      </span>
-
 
      {#await promise}
           <pre><HTML_Marquee value="🐘" /></pre>
@@ -81,7 +61,6 @@
      {/await}
      
 </div>
-
 
 <style>
      .grid-container {
