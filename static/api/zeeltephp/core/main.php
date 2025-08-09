d77802a87227dd43a7033aa198091a7a363d8e74
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-08-10 00:50:32 +0200

    response layer, context

--- static/api/zeeltephp/core/main.php
+++ static/api/zeeltephp/core/main.php
@@ -4,6 +4,8 @@ use function ZeeltePHP\Core\Lib\load_DotEnv_file;
 use function ZeeltePHP\Core\Lib\load_lib_files;
 use function ZeeltePHP\Error\log_debug;
 use function ZeeltePHP\Error\handle_error;
+
+use stdClass;
 use ZeeltePHP\Lib\DB\ZP_DB;
 
 /***
@@ -35,6 +37,7 @@ function main() {
 
                // # init ZP_DB provider (if set)
                if (isset($env['ZEELTEPHP_DATABASE_URL'])) {
+                    log_debug('  load ZP_DB');
                     require_once('lib/db/db.db.php');
                     $db = new ZP_DB($env['ZEELTEPHP_DATABASE_URL']);
                }
@@ -60,75 +63,97 @@ function main() {
                     */
                }
                
-               // the response of the +server files is passed the route downwards
-               $data = null;
-               $data = $zpAR->routeFiles;
-               
+               $data  = null;
+               $form  = null;
+               $error = null;
+
                // parse the route
                foreach ($zpAR->routeFiles as $plusPhpFile) {
                     $zpTime->start($plusPhpFile);
                     log_debug("+server:    $plusPhpFile", 2);
-                    log_debug(str_starts_with($plusPhpFile, '+page.server.') ? 'yes':'no', 14);
 
-                    $zpns = '';
+                    $zpns    = '';
+                    $options = [
+                         'db'     => $db,
+                         'env'    => $env,
+                         'zpAR'   => $zpAR,
+                         'params' => $_REQUEST
+                    ];
+
                     include("$zpAR->routeBase/$plusPhpFile");
                     if ($zpns !== '') {
-                         log_debug("  has namespace");
+                         log_debug("  has namespace $zpns");
+                         if ($zpAR->context == 'page') {
+                              log_debug("  is context 'page' ");
 
-                         if ($zpAR->context == 'api') {
-                              log_debug("  is context == 'api'");
-                              include_once('core/exec.server.php');
-                              $data = \ZeeltePHP\Core\Exec\exec_PlusServer($zpns);
-                         }
-                         else { 
-                              log_debug("  is context == 'page'");
-                              
                               if (str_starts_with($plusPhpFile, '+layout.server.')) {
-                                   log_debug("  loaded +layout.server");
+                                   log_debug("  load +layout.server");
                                    include_once('core/exec.layout.server.php');
-                                   $data = \ZeeltePHP\Core\Exec\exec_PlusLayoutServer($zpns);
+                                   //$data = \ZeeltePHP\Core\Exec\exec_PlusLayoutServer($zpns, $options);
+                                   log_debug("data");
+                                   log_debug($data);
                               }
                               else if (str_starts_with($plusPhpFile, '+page.server.')) {
-                                   log_debug("  loaded +page.server");
+                                   log_debug("  load +page.server");
                                    include_once('core/exec.page.server.php');
-                                   $data = \ZeeltePHP\Core\Exec\exec_PlusPageServer($zpns);
+                                   $response = \ZeeltePHP\Core\Exec\exec_PlusPageServer($zpns, $options);
+                                   log_debug("  response");
+                                   log_debug($response);
+                                   $data  = $response->data  ?? $data;
+                                   $form  = $response->form  ?? $form;
+                                   $error = $response->error ?? $error;
                               }
                               else {
                                    log_debug("  no +server file match for $zpAR->routeBase/$plusPhpFile");
                               }
                          }
-
+                         else if ($zpAR->context == 'api') {
+                              log_debug("  is context 'api' ");
+                              include_once('core/exec.server.php');
+                              $data = \ZeeltePHP\Core\Exec\exec_PlusServer($zpns, $options);
+                         }
                     } else {
                          # log error at current route and exit loop (root cause) 
                          $data = null; # just to be sure to not expose from parent data
-                         log_debug("  missing namespace for $zpAR->routeBase/$plusPhpFile");
+                         log_debug("  ! missing namespace for $zpAR->routeBase/$plusPhpFile !");
                          break;
                     }
-
                     log_debug($zpTime->endN($plusPhpFile));
                }
 
                // return data as JSON response, and let zp_fetch_api() do the rest :-)
-               echo json_encode($data);
                /*
-               from 1.0.4 only return only the data and use response-codes - no overhead-layer anymore.
-               echo json_encode([
-                    'ok'   => true,
-                    'code' => 200,
-                    'data' => $data,
-                    //'zpDB' => $db
-                    // -- ...$data // pitfall - do not because of send-data-JSON of any data  
-               ]);
+                    from 1.0.4 only return only the data and use response-codes - no overhead-layer anymore.
+                    echo json_encode([
+                         'ok'   => true,
+                         'code' => 200,
+                         'data' => $data,
+                         //'zpDB' => $db
+                         // -- ...$data // pitfall - do not because of send-data-JSON of any data  
+                    ]);
                */
-          } 
-          else {
-               // no +server files found in route
-               // previous 1.0.4 we returned an error, from now just send an default empty-JSON response
-               echo json_encode((object)null);
+               log_debug('response is context '.$zpAR->context);
+               $response;
+               if ($zpAR->context == 'page') {
+                    $response = new stdClass();
+                    $response->page  = $zpAR->context;
+                    $response->form  = $form ;
+                    $response->error = $error;
+                    $response->data  = $data ;
+               } else if ($zpAR->context == 'api') {
+                    $response = $data;
+               }
           }
+
+          // no +server files found in route
+          // previous 1.0.4 we returned an error, from now just send an default empty-JSON response
+          //echo json_encode((object)null);
+          $jsonResponse = json_encode($response, JSON_PRETTY_PRINT, 4);
+          log_debug($jsonResponse);
+          echo $jsonResponse;
+
           log_debug('//zeeltephp_main()');
           log_debug($zpTime->endN('main()'));
-
      } catch (\Error $exp) {  
           handle_error($exp); //, "Error");
      } catch (\Exception $exp) {
