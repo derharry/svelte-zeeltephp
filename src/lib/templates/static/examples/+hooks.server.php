a0893fa87d7fb419fea4980dcda44bd93a338a6a
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-08-17 20:42:05 +0200

    introduce example of +hooks.server.php

---
+++ src/lib/templates/static/examples/+hooks.server.php
@@ -0,0 +1,27 @@
+<?
+
+     /**
+      * @param object   $event 
+      * @param callable $resolve
+      */
+     function handle($event, callable $resolve) {
+          global $zpAR, $env, $db;
+
+          //
+          // ... do any custom implementation
+          //
+
+          // required to execute /routes/ 
+          // or don't to bypass ZeeltePHP
+          $response = $resolve();
+
+          // customize response or headers
+          // expose zp-dev-vars globally
+          $response->_zpAR  = $zpAR;
+          $response->_zpENV = $env;
+          $response->_zpENV = $env;
+
+          return $response;
+     }
+
+?>
