1496fadd2efd579134d52cc03c25fee811f8d5a1
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-08-17 21:50:13 +0200

    example +server.php

---
+++ src/routes/api/usage_example/+server.php
@@ -0,0 +1,19 @@
+<?php namespace zp1753838398; $zpns=__NAMESPACE__;
+
+     function fallback() {
+          return 'hi from fallback()';
+     }
+
+     function GET() {
+          return 'hi from GET';
+     }
+     
+     function PUT() {
+          return 'hi from PUT';
+     }
+     
+     function DELETE() {
+          return 'hi from DELETE';
+     }
+     
+?>
