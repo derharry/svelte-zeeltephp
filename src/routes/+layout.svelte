f17956f435770bbacf1d5616b0c9f121214795e9
Harry <10669513+derharry@users.noreply.github.com10669513+derharry@users.noreply.github.com>
2025-08-27 21:29:58 +0200

    layout: change of layout for new documentation and Components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,25 @@
 <script>
-      import '@picocss/pico'
+      //import '@picocss/pico'
       import '../app.css'
       import { Navigation } from "zeelte";
 
       let { children } = $props();
 
       const menu = [
-            { label: 'ZeeltePHP',      url: '/'               },
-            { label: 'Documentation',  url: '/docs'           },
-            //{ label: 'Zeelte',         url: '/docs_zeelte'    },
+            { label: 'Documentation',  url: '/docs/introduction' },
+            { label: 'Lib',            url: '/docs_lib'            },
+            { label: 'ZPDev',          url: '/zpdev'               },
             //{ label: 'Usage Examples', url: '/zp_fetch_PAGE'  },
-            { label: 'ZPDev',          url: '/zpdev'          },
             //{ label: 'What Else',      url: '/what_else'      }
       ];
 
 </script>
 
 <main>
-      <Navigation data_nav={menu} />
+      <Navigation data_nav={menu}>
+            <div slot="logo"><a href="/">ZeeltePHP</a></div>
+            <div slot="right"><input type="search"></div>
+      </Navigation>
       <article>
             {@render children()}
       </article>
